7f63b833626c9d2d175bffb39a18fe92c4e06d65
EdwinArevalo <edwin.arevalo@tecsup.edu.pe>
2020-03-03 11:40:01 -0500

    first commit

---
+++ formulario.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario1</title>
+</head>
+<body>
+
+    <form method="POST" action="sumar.php">
+    <p>Valor 1: <input type="text" name="T1" size="20"> </p> 
+    <p>Valor 2: <input type="text" name="T2" size="20"></p>
+    <p><input type="submit" name="B1" value="Sumar"></p>
+    </form>
+</body>
+</html>
---
+++ sumar.php
@@ -0,0 +1,10 @@
+<?php
+
+    $valor1 = $_POST['T1'];
+    $valor2 = $_POST['T2'];
+
+    $suma = $valor1 + $valor2;
+
+    echo "$valor1 + $valor2 = $suma";
+
+?>
